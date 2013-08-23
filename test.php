acc5d70a084e57fb51f822255aec8ba541b777b4
Lauri <lauri.naalisvaara@magneetto.com>
2013-08-23 11:13:46 +0300

    testikommentti 11:10

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
  * Created by JetBrains PhpStorm.
  * User: Lauri
  * Date: 23.8.2013
- * Time: 11:09
+ * Time: 11:09sadasd
  * To change this template use File | Settings | File Templates.
  */
 echo "moro";
